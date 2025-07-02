38a706ec94712fc82817f543cc65a3f2eae73ed9
tighwm <partnyor117@gmail.com>
2025-07-02 13:49:51 +0300

    added novel views

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from api.v1.views.auth import router as auth_router
 from api.v1.views.user import router as user_router
+from api.v1.views.novel import router as novel_router
 
 router = APIRouter(prefix="/v1")
 router.include_router(auth_router)
 router.include_router(user_router)
+router.include_router(novel_router)
--- src/api/v1/views/novel.py
+++ src/api/v1/views/novel.py
@@ -1,18 +1,31 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.v1.schemas.novel import NovelRead, NovelCreate
+from api.v1.utils import get_current_user_by_token
+from api.v1.services import novel as novel_serv
+from core.database.models import db_helper
+
 
 router = APIRouter(
-    prefix="/novel/",
+    prefix="/novel",
     tags=["Novel"],
 )
 
 
 @router.post(
-    "/",
+    "/novel/",
     response_model=NovelRead,
 )
-async def handle_create_novel(
+async def handle_create(
     novel_in: NovelCreate,
+    access_token: str,
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
 ):
-    pass
+    user = await get_current_user_by_token(access_token, session)
+    return await novel_serv.create_novel(novel_in, user, session)
