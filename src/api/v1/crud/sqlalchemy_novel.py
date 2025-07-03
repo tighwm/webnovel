baee6851144d84f9a3e2a241fe885d9f60fd72db
tighwm <partnyor117@gmail.com>
2025-07-03 18:18:59 +0300

    added get_novel_by_id and update_novel

--- src/api/v1/crud/sqlalchemy_novel.py
+++ src/api/v1/crud/sqlalchemy_novel.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.database.models import Novel
-from api.v1.schemas.novel import NovelCreate
+from api.v1.schemas.novel import NovelCreate, NovelPartial, NovelUpdate
 
 
 async def create(
@@ -12,4 +12,23 @@ async def create(
     session.add(novel)
     await session.commit()
     await session.refresh(novel)
+    return novel
+
+
+async def get_novel_by_id(
+    session: AsyncSession,
+    novel_id: int,
+) -> Novel | None:
+    return await session.get(Novel, novel_id)
+
+
+async def update_novel(
+    session: AsyncSession,
+    novel: Novel,
+    novel_in: NovelUpdate | NovelPartial,
+    partial: bool = False,
+):
+    for name, value in novel_in.model_dump(exclude_unset=partial).items():
+        setattr(novel, name, value)
+    await session.commit()
     return novel
