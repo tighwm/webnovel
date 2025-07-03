1f42b26d8b90d2d58ee3554e933eb2378b52a5e9
tighwm <partnyor117@gmail.com>
2025-07-03 18:21:36 +0300

    added get_novel and update_novel in novel service

--- src/api/v1/services/novel.py
+++ src/api/v1/services/novel.py
@@ -2,9 +2,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.v1.crud import sqlalchemy_novel as novel_crud
 from api.v1.crud import sqlalchemy_usernovelrole as unr_crud
-from api.v1.schemas.novel import NovelCreate, NovelRead
+from api.v1.schemas.novel import NovelCreate, NovelRead, NovelUpdate
 from api.v1.schemas.usernovelrole import UserNovelRoleCreate
-from core.database.models import User
+from api.v1.utils import exc_404
+from core.database.models import User, Novel
 
 
 async def create_novel(
@@ -18,5 +19,27 @@ async def create_novel(
         user_id=user.id,
         role_id=1,
     )
-    unr = await unr_crud.create(session, unr_create)
+    await unr_crud.create(session, unr_create)
+    return NovelRead.model_validate(novel)
+
+
+async def get_novel(
+    novel_id: int,
+    session: AsyncSession,
+):
+    novel = await novel_crud.get_novel_by_id(
+        session=session,
+        novel_id=novel_id,
+    )
+    if novel is None:
+        raise exc_404("Novel not found")
+    return novel
+
+
+async def update_novel(
+    novel: Novel,
+    novel_in: NovelUpdate,
+    session: AsyncSession,
+):
+    novel = await novel_crud.update_novel(session, novel, novel_in)
     return NovelRead.model_validate(novel)
