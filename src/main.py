efd8e5e0c83c1a53bd782cccff7fd8185f7077f4
tighwm <partnyor117@gmail.com>
2025-07-04 21:19:15 +0300

    my dumb ass... forgot it add to previous commit

--- src/main.py
+++ src/main.py
@@ -7,9 +7,13 @@ from api import router as api_router
 from core.database.models import db_helper
 from core.config import settings
 
+from db_role_init import init_roles
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    async with db_helper.session_getter() as session:
+        await init_roles(session)
     yield
     await db_helper.dispose()
 
